005f46c3b03587acf400cdc6fcea57bf7a1c9bb2
Jeffcheung205 <disneyland6029@gmail.com>
2025-11-24 15:20:26 +0800

    Add files via upload

---
+++ PE081DS9_CheungMongNok_14_Assignment 2.py
@@ -0,0 +1,15 @@
+''' Course Code: PE081DS9
+English Full Name: Cheung Mong Nok
+Student Number: 14
+Assignment Numeber: 2'''
+
+print("Type of fruit with five letter and red in color")
+
+# case: user's answer was correct
+while attem:
+    guess = input("Your guess: ") # to let user input their guess
+    if guess == "aPPle":  # while user's guess correct
+        print("Bingo!")
+        break
+    else:  # while user's guess was incorrect
+        print("Wrong guess, try again!")
