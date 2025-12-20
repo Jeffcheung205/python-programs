04ad1f6b8a67af7f57622febdb0c0801bb1ba90c
Jeffcheung205 <disneyland6029@gmail.com>
2025-12-20 14:33:37 +0800

    add a print triangle through "*" program

---
+++ traingle.py
@@ -0,0 +1,9 @@
+# traingle program
+rows =6
+
+for i in range(rows):
+    # for j in range(rows - i - 1):
+    #     print(" ", end="")
+        for j in range(2 * i+ 1):
+            print("*", end="")
+        print()
